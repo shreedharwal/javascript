3ecddcf9a60b854a688b53c0a4b9dd32167e77e1
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-05-11 14:25:36 +0000

    Conversion of datatypes in javaScript

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+// let score= "33abc" // NaN
+// let score= null //0
+// let score= undefined // NaN
+// let score= true //1
+// let score= 'shreya' //NaN
+
+// console.log(typeof score)
+
+// let valueInNum= Number(score)
+// console.log(typeof(valueInNum));
+// console.log(valueInNum);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+
+// let isLoggedIN= "shreya"
+// let booleanIsLoggedIN= Boolean(isLoggedIN)
+// console.log(booleanIsLoggedIN);
+
+
+//1=> true viceversa for false
+// ""=> false
+// "shreya"=> true
+
+let someNum= 33
+let stringNum= String(someNum)
+console.log(typeof stringNum);
