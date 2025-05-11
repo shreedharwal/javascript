a2cc664e782966e501947ea7af35df3b693d7a38
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-05-11 14:57:41 +0000

    INtro to Conversuon Operations in JavaScript

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -24,6 +24,40 @@
 // ""=> false
 // "shreya"=> true
 
-let someNum= 33
-let stringNum= String(someNum)
-console.log(typeof stringNum);
+// let someNum= 33
+// let stringNum= String(someNum)
+// console.log(typeof stringNum);
+
+//**********************Operations*******************************/
+// 
+
+// let str1="hello"
+// let str2=" shreya"
+// console.log(str1+str2);
+
+// console.log("1"+ 2)
+// console.log("1"+(2+ 2)) //make the use of parentheses
+// console.log(1+2+"2")
+
+// console.log(+""); //0
+
+// let num1,num2,num3
+// num1=num2=num3=2+2 //no readability
+
+// let gamecounter=100
+// gamecounter++
+// // console.log(gamecounter)
+// // ++gamecounter
+// console.log(gamecounter)
+
+
+//*****************Prefix and Postfix Increment**********************/
+//Increment++ (Postfix:If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.)
+//  let x=5
+//  const y=x++
+//  console.log(x,y) //6,5
+
+// ++Increment (Prefix:If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.)
+ let x=5
+ const y= ++x
+ console.log(x,y) //6,6
