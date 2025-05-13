6d100ebfacc00eb640d7fdd6d4836b599f808765
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-05-13 17:26:14 +0000

    More About Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,36 @@
+// const name= "shreya"
+// const repcount=50
+//console.log(name+ repcount+ "value") outdated method
+
+//string interpolation used as
+//console.log(`hello my name is ${name.toUpperCase()} and my repo count is ${repcount}`)
+
+//new way to declare string 
+const Name= new String('shreya-Dh') // here string is object
+// console.log(typeof(Name))//object
+// console.log(Name[0])
+// console.log(Name.length);
+//console.log(Name.toUpperCase()); // string is a primitive datatype and it is stored in a stack
+//console.log(Name.charAt(5)); //char at index 5 is a
+//console.log(Name.indexOf('y')); // give index of character
+
+// const newString= Name.substring(0,4)
+// console.log(newString);
+
+//const anotherStr= Name.slice(0,4) //can give negative values also
+// console.log(anotherStr);
+
+const anotherStr= Name.slice(-2,9)
+console.log(anotherStr);
+
+
+console.log("hello");
+//need to continue from this 
+
+
+
+
+
+
+
+
