891f96c5cd9d26e6b8a3aa1a6568d8291740788a
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-05-14 16:45:36 +0000

    More Functions/Methods of String

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -6,7 +6,8 @@
 //console.log(`hello my name is ${name.toUpperCase()} and my repo count is ${repcount}`)
 
 //new way to declare string 
-const Name= new String('shreya-Dh') // here string is object
+const Name= new String('shreya-Dh-com') // here string is object
+
 // console.log(typeof(Name))//object
 // console.log(Name[0])
 // console.log(Name.length);
@@ -14,18 +15,29 @@ const Name= new String('shreya-Dh') // here string is object
 //console.log(Name.charAt(5)); //char at index 5 is a
 //console.log(Name.indexOf('y')); // give index of character
 
-// const newString= Name.substring(0,4)
+// const newString= Name.substring(0,4)//does not take negative indexes
 // console.log(newString);
 
 //const anotherStr= Name.slice(0,4) //can give negative values also
 // console.log(anotherStr);
 
-const anotherStr= Name.slice(-2,9)
-console.log(anotherStr);
+// const anotherStr= Name.slice(-2,9)
+// console.log(anotherStr);
 
 
-console.log("hello");
-//need to continue from this 
+// const  newStrone="  shreya  "
+// console.log(newStrone)
+// console.log(newStrone.trim());//basically trims the unwanted spaces
+
+
+// const url="https://shreya.com/shreya%20dharwal"
+// console.log(url.replace('%20', '-')) //%20 is a whitespace char so if we need to replace that we can do it by replace()
+
+// console.log(url.includes('shreya'))
+// console.log(url.includes('somi'))//tells if this is present or not
+
+console.log(Name.split('-'))//if want to seprate values on basis of something, gives in form of array  
+
 
 
 
