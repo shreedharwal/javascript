5cbece7a758d2f8243dde522c1f3e95ecb3e1e79
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-05-15 17:16:34 +0000

    INtroduction to Dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,21 @@
+//Dates
+let Mydate= new Date()
+// console.log(Mydate);//2025-05-15T16:46:45.355Z
+// console.log(Mydate.toString());//Thu May 15 2025 16:47:01 GMT+0000 (Coordinated Universal Time)
+// console.log(Mydate.toDateString());//Thu May 15 2025
+// console.log(Mydate.toISOString());//2025-05-15T16:46:09.962Z
+//console.log(Mydate.toJSON());//2025-05-15T16:47:33.958Z
+// console.log(Mydate.toLocaleTimeString())// 4:48:38 PM
+// console.log(Mydate.toLocaleString())// 5/15/2025, 4:48:38 PM
+
+// console.log(typeof(Mydate));// object
+
+//to declare a my created date
+// let mycreatedDate= new Date(2025,5,2)//months starts form 0
+// console.log(mycreatedDate.toDateString());
+
+
+
+
+
+
