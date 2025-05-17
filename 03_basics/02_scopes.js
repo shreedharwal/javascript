7c15aaf5c16ef873d6f1e7d0132d637e021e0be9
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-05-17 16:56:21 +0000

    ABout scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,99 @@
+//let var const
+//dont use var
+
+
+// let a= 10
+// const b=20
+// var c=30
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// let a =300
+// if(true){
+//     let a=10
+//     const b=20
+//     var c=30
+// }
+// // console.log(a);//gives error
+// // console.log(b);//error
+// console.log(c);// 30//here it is printed
+
+
+//global scope
+// let a =300
+// //block scope
+// if(true){
+//     let a=10
+//     const b=20
+//     console.log('INNER:', a)
+// }
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
