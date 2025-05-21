f9f77a485d6432683f997afea814e394fd4c9ae1
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-05-21 14:34:21 +0000

    About for of loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,76 @@
+//for of
+
+// const arr=[1,2,3,4,5]
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+
+// const greetings='hello world'
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`); 
+// }
+
+
+//maps
+// const map = new Map()// unique values
+// map.set('IN', 'India')
+// map.set('USA', 'United states')
+// map.set('Fr', 'France')
+// map.set('IN', 'India')
+// console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+// }
+
+
+// const myobj={
+//     'game1': 'NFS',//object is not iteratable in for of
+//     'game2':'spiderman'
+// }
+
+// for (const [key, value] of myobj) {
+//     console.log(key, ':-', value);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
