e70af6cbdaef00935caec029ffe69b2d5a48764b
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-05-21 08:51:25 +0000

    Break and continue

---
+++ 05_iterations/one.js
@@ -0,0 +1,116 @@
+//for 
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if(element == 5){
+//         console.log('5 is best num');
+//     }
+//     console.log(element)
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop: ${i}`)
+//     for (let j = 1; j <= 10; j++) {
+//         //console.log(`inner loop ${j} and inner loop ${i}`)
+//         console.log(i + '*' + j + '=' + i*j)
+//         }
+    
+// }
+
+// myarr=['flash', 'batman','superman']
+
+// for (let index = 0; index < myarr.length; index++) {
+//     const element = myarr[index];
+//     console.log(element)
+// }
+
+
+//break, continue
+// for (let index = 1; index <= 20; index++) {
+//     if (index==5){
+//         console.log('detected 5')
+//         break
+//     }
+//     console.log(`val of i is ${index}`)
+    
+// }
+
+
+for (let index = 1; index <= 20; index++) {
+    if (index==5){
+        console.log('detected 5')
+        continue
+    }
+    console.log(`val of i is ${index}`)
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
