e5d6962d5b86097dc72f17f71a9fcaeaef318346
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-05-21 06:57:04 +0000

    About switch case

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,126 @@
+//basic syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month=5
+switch (month) {
+    case 1:
+        console.log('january');
+        break;
+    case 2:
+        console.log('feb');
+        break;
+    case 3:
+        console.log('march');
+        break;
+    case 4:
+        console.log('april');
+        break;
+    case 5:
+        console.log('may');
+        break;
+    default:
+        console.log('default case match');
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
