8086a1ee97e5d0a7a0329049c1d0cb2fac355f36
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-05-21 06:47:38 +0000

    basics of if statement

--- 04_controlflow/one.js
+++ 04_controlflow/one.js
@@ -1,4 +1,59 @@
 //if
-if(true){
-    
-}
+//<,>, <=, >=, ==, !=, ===(for type, strict equal), !==
+
+// const temp= 41
+// if (temp<50){
+//     console.log('less than 50');
+// }
+// else{
+//     console.log('temp greater than 50')
+// }
+
+
+// if(2 === '2'){
+//     console.log("executed")
+// }
+
+
+// const score= 200
+// if(score>100){
+//     const power='fly'
+//     console.log(`user power: ${power}`);
+//     }
+
+
+//shorthand notation
+//implicit scope
+// const balance=1000
+// if(balance > 500) console.log('test');
+
+
+//nesting
+// const balance=1000
+// if(balance < 500){
+//     console.log('less than');
+// }
+// else if (balance < 750){
+//     console.log('less than 750');
+// }
+// else if(balance < 900){
+//     console.log('less than 750');
+// }
+// else{
+//     console.log('less than 1200');
+// }
+
+
+// const userLoggesin= true
+// const debitcard= true
+// const loggedInFromgoogle= false
+// const loggedInFromEmail= true
+
+// if(userLoggesin && debitcard){
+//     console.log('allow to buy');
+// }
+
+// if(loggedInFromgoogle || loggedInFromEmail){// who so ever is true, the user got loggedin
+//     console.log('user logged in');
+// }
+
