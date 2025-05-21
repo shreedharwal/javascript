1218048d69136634bb99a7f6c2afc8afd46d1248
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-05-21 14:51:29 +0000

    About for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,45 @@
+//for in
+
+// const myobj= {
+//     js:'javascript',
+//     cpp:'c++',
+//     rb:'ruby',
+//     swift:'swift by apple'
+// }
+
+// for (const key in myobj) {
+//     // console.log(key);
+//     //console.log(myobj[key]);
+//     console.log(`${key} shortcut is for ${myobj[key]}`)
+// }
+
+// const programming= ["js","rb", "py", "java", "swift"]
+// for (const key in programming) {
+//     console.log(programming[key]);
+    
+// }
+
+
+
+const map = new Map()// unique values
+map.set('IN', 'India')
+map.set('USA', 'United states')
+map.set('Fr', 'France')
+map.set('IN', 'India')
+// console.log(map);
+
+for (const key in map) {
+    console.log(map[key]);//not iterable
+    
+}
+
+
+
+
+
+
+
+
+
+
+
