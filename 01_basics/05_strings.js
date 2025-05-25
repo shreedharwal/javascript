07bc83153ed67b0e8b905d18ba9e4efdab539cbe
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-05-25 12:42:12 +0000

    Intro to string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -11,14 +11,14 @@ const Name= new String('shreya-Dh-com') // here string is object
 // console.log(typeof(Name))//object
 // console.log(Name[0])
 // console.log(Name.length);
-//console.log(Name.toUpperCase()); // string is a primitive datatype and it is stored in a stack
-//console.log(Name.charAt(5)); //char at index 5 is a
-//console.log(Name.indexOf('y')); // give index of character
+// console.log(Name.toUpperCase()); // string is a primitive datatype and it is stored in a stack
+// console.log(Name.charAt(5)); //char at index 5 is a
+// console.log(Name.indexOf('y')); // give index of character
 
 // const newString= Name.substring(0,4)//does not take negative indexes
 // console.log(newString);
 
-//const anotherStr= Name.slice(0,4) //can give negative values also
+// const anotherStr= Name.slice(0,4) //can give negative values also
 // console.log(anotherStr);
 
 // const anotherStr= Name.slice(-2,9)
@@ -31,12 +31,12 @@ const Name= new String('shreya-Dh-com') // here string is object
 
 
 // const url="https://shreya.com/shreya%20dharwal"
-// console.log(url.replace('%20', '-')) //%20 is a whitespace char so if we need to replace that we can do it by replace()
+// // console.log(url.replace('%20', '-')) //%20 is a whitespace char so if we need to replace that we can do it by replace()
 
 // console.log(url.includes('shreya'))
 // console.log(url.includes('somi'))//tells if this is present or not
 
-console.log(Name.split('-'))//if want to seprate values on basis of something, gives in form of array  
+// console.log(Name.split('-'))//if want to seprate values on basis of something, gives in form of array  
 
 
 
