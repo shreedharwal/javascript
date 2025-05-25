21c14ff500569b55bfa88ad2d03505dfe7d988d8
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-05-25 12:46:55 +0000

    Intro to num and maths

--- 01_basics/06_num_and_maths.js
+++ 01_basics/06_num_and_maths.js
@@ -4,7 +4,7 @@
 // const balance= new Number(100)
 // console.log(balance);
 
-// console.log(typeof(balance.toString()))
+// console.log(typeof(balance.toString()))//string
 // console.log(balance.toString().length)
 // console.log(balance.toFixed(2));//it fix the number upto 2 decimal places
 
@@ -13,7 +13,7 @@
 // console.log(otherNum.toPrecision(3));//Returns a string containing a number represented either in exponential or fixed-point notation with a specified number of digits.
 
 // const hundred=1000000003456789
-// console.log(hundred.toLocaleString());//adds comas for very large numbers
+// console.log(hundred.toLocaleString());//adds commas for very large numbers
 //default is in US standard ..can convert to indian value also
 // console.log(hundred.toLocaleString('en-IN'))
 
@@ -34,9 +34,9 @@
 // console.log((Math.random()*100)+1);//for getting values above 0 and 1
 
 //to avoid getting 0 and getting a number in range of min and max
-const min=10
-const max=20
-console.log(Math.floor(Math.random() * (max - min +1)) + min)
+// const min=10
+// const max=20
+// console.log(Math.floor(Math.random() * (max - min +1)) + min)
 
 
 
