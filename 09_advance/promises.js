70fe4e82a048d4cd83f643ba1cd5e7fd6e464e51
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-06-01 14:56:51 +0000

    About async-await, .then(), .fetch(), try-catch block

--- 09_advance/promises.js
+++ 09_advance/promises.js
@@ -36,29 +36,76 @@
 // })
 
 
-const promiseFour= new Promise( function(resolve,reject){
+// const promiseFour= new Promise( function(resolve,reject){
+//     setTimeout(function(){
+//         let error= true
+//         if(!error){
+//             resolve({username:"shreya", email:'shreyadharwal@gmail.com'})
+//         }else{
+//             reject("ERROR: Something Went Wrong")
+//         }
+//     },1000)
+// })
+
+// const username= promiseFour.then( (user) => {
+//     console.log(user);
+//     return user.username
+//     //callback hell
+// }).then( (username) => {
+//     console.log(username);
+    
+// }).catch(function(error){
+//     console.log(error)
+// }).finally(()=>{
+//     console.log("The promise is either resolved or rejected");
+    
+// })
+
+
+const promiseFive= new Promise(function(resolve, reject){
     setTimeout(function(){
         let error= true
         if(!error){
-            resolve({username:"shreya", email:'shreyadharwal@gmail.com'})
+            resolve({username:"javascript", password:"12345",email:'shreyadharwal@gmail.com'})
         }else{
-            reject("ERROR: Something Went Wrong")
+            reject("ERROR: Js Went Wrong")
         }
     },1000)
+});
+
+async function consumepromiseFive(){
+    try{
+        const response= await promiseFive
+        console.log(response);
+    }catch(error){
+        console.log(error)
+    }
+    
+}
+consumepromiseFive()
+
+// async function getallUsers(params) {
+//     try {
+//         const response =await fetch("https://jsonplaceholder.typicode.com/users")
+//         const data= await response.json()
+//         console.log(data)
+//     } catch (error) {
+//         console.log('E: ', error);
+        
+//     }
+// }
+// getallUsers()
+
+//by .then. catch
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
+}).then((data)=>{
+    console.log(data);
 })
-
-const username= promiseFour.then( (user) => {
-    console.log(user);
-    return user.username
-    //callback hell
+.catch((error)=>{
+    console.log('error');
 })
-console.log(username);
-
-//to be continued from here
-
-
-
-
 
 
 
