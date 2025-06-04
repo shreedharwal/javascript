949f04c542fca3192e78653262d48ea0307720ca
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-06-04 09:32:05 +0000

    basics of oops

--- 10_oops/oop.js
+++ 10_oops/oop.js
@@ -3,23 +3,44 @@
 const user={
     username:'shreya',
     loginCount:8,
-    signedIN:true,
+    signedIN:false,
 
     getUserDetails: function(){
-        console.log("got user details from db");
-        
+        // console.log("got user details from db");
+        // console.log(`username: ${this.username}`);
+        //console.log(this);// gives current context
+            
     }
 }
-console.log(user.username);
-console.log(user.getUserDetails());
+// console.log(user.username);
+//console.log(user.getUserDetails());
+// console.log(this);//global context always changes
 
 
+//Constructor function(new keyword- to create multiple instances)
 
+function User(username, loginCount, isLoggedin){
+    this.username = username
+    this.loginCount=loginCount
+    this.isLoggedin=isLoggedin
 
+    this.greeting= function(){
+        console.log(`welcome: ${this.username}`);
+        
+    }
 
+}
 
+const userOne= new User("shreya", 1, true)
+const userTwo= new User("somi", 2,false)
+console.log(userTwo.constructor);//[Function: User]
+// console.log(userTwo)
+// console.log(userOne);
 
-
+// new keyword - Empty object is created also known as instance{}
+// Constructor function is called through new keyword
+// All the arguments got injected in the new keyword
+// got all the values
 
 
 
